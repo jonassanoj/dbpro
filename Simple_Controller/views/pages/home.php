dd81ad8e84596c48faf3c5db3ae9937e73338988
ahmadnawid mustafazada <ahmadnawid.mz@gmail.com>
2012-05-16 13:18:13 +0200

    Just a simple Controller for main and static pages.

---
+++ Simple_Controller/views/pages/home.php
@@ -0,0 +1,17 @@
+<center>
+<?php
+
+//echo "This is the Home Page.";
+
+?>
+<div style="position: absolute; left: 50px; top: 150px;">
+
+
+
+<textarea rows="30" cols="80">
+Here you can find list of the recent questions with little description. 
+</textarea>
+</div>
+<div align="right">
+<input type="text" value="search question" />
+<input type="submit" value="Ask a Question" />
---
+++ Simple_Controller/views/templates/footer.php
@@ -0,0 +1,15 @@
+<div style="position: absolute; left: 600px; bottom: 20px;">
+
+<a href="about">About Us</a>
+ | 
+<a href="about">Help</a>
+ | 
+<a href="about">FAQ</a>
+ | 
+<a href="about">Contact</a>
+
+
+
+<strong>&copy; 2012 </strong>
+</body>
+</html>
