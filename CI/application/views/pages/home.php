4de7d130b60c0f5dbc59bd625317005f9777d790
Tanin <tanin@tanin-pc.(none)>
2012-06-05 14:43:06 +0200

    search.png added

--- CI/application/views/pages/home.php
+++ CI/application/views/pages/home.php
@@ -7,7 +7,7 @@
 </form>
 <div class="HorizLinks">
 <ul>
-<li><a href="homel.php">Home</a></li>
+<li><a href="home.php">Home</a></li>
 <li><a href="news.php">News</a></li>
 <li><a href="about.php">About Us</a></li>
 <li><a href="contact.php">Contact Us</a></li>
--- CI/application/views/templates/header.php
+++ CI/application/views/templates/header.php
@@ -1,14 +1,13 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+
+<link rel="stylesheet" href="<?php echo base_url();?>css/style.css" type="text/css" media="screen"/>
 
 	<title><?php echo $title ?> goftogo forum</title>
 
 </head>
 <body>
 	
-
-
 
 
 
