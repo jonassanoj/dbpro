a2f9ff5dcb302aaf40a876d3210617a518030063
jonas <jonas.bucher@campus.tu-berlin.de>
2012-06-06 13:59:09 +0200

    fixed the project

--- dbDumps/goftogo.sql
+++ dbDumps/goftogo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 06, 2012 at 12:04 AM
+-- Generation Time: Jun 06, 2012 at 01:48 AM
 -- Server version: 5.5.22
 -- PHP Version: 5.3.10-1ubuntu3.1
 
@@ -27,17 +27,17 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `Answer` (
-  `answerID` int(11) NOT NULL,
-  `questionID` int(11) NOT NULL,
-  `answeredDate` date NOT NULL,
+  `answerID` int(11) NOT NULL AUTO_INCREMENT,
+  `questionID` int(11) DEFAULT NULL,
+  `date` date NOT NULL,
   `rank` int(11) NOT NULL DEFAULT '0',
   `body` text NOT NULL,
-  `userID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
   PRIMARY KEY (`answerID`),
-  UNIQUE KEY `answeredDate` (`answeredDate`),
+  UNIQUE KEY `answeredDate` (`date`),
   KEY `Question_ID_fk_Answer` (`questionID`),
   KEY `Answer_userID_FK` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -45,11 +45,11 @@ CREATE TABLE IF NOT EXISTS `Answer` (
 -- Table structure for table `Catagory`
 --
 
-CREATE TABLE IF NOT EXISTS `Catagory` (
-  `catID` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Category` (
+  `catID` int(11) NOT NULL AUTO_INCREMENT,
   `catName` varchar(40) NOT NULL,
   PRIMARY KEY (`catID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -58,17 +58,18 @@ CREATE TABLE IF NOT EXISTS `Catagory` (
 --
 
 CREATE TABLE IF NOT EXISTS `Comment` (
-  `commentID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `answerID` int(11) NOT NULL,
+  `commentID` int(11) NOT NULL AUTO_INCREMENT,
+  `userID` int(11) DEFAULT NULL,
+  `answerID` int(11) DEFAULT NULL,
   `Date` date NOT NULL,
-  `commentBody` text NOT NULL,
+  `body` text NOT NULL,
   `questionID` int(11) DEFAULT NULL,
   PRIMARY KEY (`commentID`),
   KEY `Answer_ID_fk_Comment` (`answerID`),
   KEY `Comment_userID_FK` (`userID`),
   KEY `Questionid_to_comment` (`questionID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 
 -- --------------------------------------------------------
 
@@ -89,18 +90,19 @@ CREATE TABLE IF NOT EXISTS `Field` (
 --
 
 CREATE TABLE IF NOT EXISTS `Question` (
-  `questionID` int(11) NOT NULL,
-  `catID` int(11) NOT NULL,
-  `fieldID` int(11) NOT NULL,
-   `postedDate` date NOT NULL,
+  `questionID` int(11) NOT NULL AUTO_INCREMENT,
+  `catID` int(11) DEFAULT NULL,
+  `fieldID` int(11) DEFAULT NULL,
+  `postedDate` date DEFAULT NULL,
   `rank` int(11) NOT NULL DEFAULT '0',
+  `title` varchar(50) NOT NULL,
   `body` text NOT NULL,
-  `userID` int(11) NOT NULL,
+  `userID` int(11) DEFAULT NULL,
   PRIMARY KEY (`questionID`),
   KEY `Question_userID_FK` (`userID`),
   KEY `Question_fieldID_FK` (`fieldID`),
   KEY `Question_catID_FK` (`catID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -109,26 +111,25 @@ CREATE TABLE IF NOT EXISTS `Question` (
 --
 
 CREATE TABLE IF NOT EXISTS `User` (
-  `userID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `userName` varchar(20) NOT NULL,
-  `firstName` varchar(30) NOT NULL,
-  `lastName` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  `imagePath` varchar(100) NOT NULL,
-  `acountCreationDate` date NOT NULL,
-  `rank` int(11) NOT NULL DEFAULT '0',
-  `lastLogin` date NOT NULL,
-  `organization` varchar(50) NOT NULL,
-  `dateOfBirth` date NOT NULL,
-  `degree` varchar(50) NOT NULL,
-  `fieldID` int(11) NOT NULL,
-  `userTypeID` int(11) NOT NULL,
-  `detail` text NOT NULL,
+  `fullName` varchar(30) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(120) NOT NULL,
+  `imagePath` varchar(100) DEFAULT NULL,
+  `acountCreationDate` date DEFAULT NULL,
+  `rank` int(11) DEFAULT '0',
+  `lastLogin` date DEFAULT NULL,
+  `organization` varchar(50) DEFAULT NULL,
+  `dateOfBirth` varchar(30) DEFAULT NULL,
+  `degree` varchar(50) DEFAULT NULL,
+  `fieldID` int(11) DEFAULT NULL,
+  `userTypeID` int(11) DEFAULT NULL,
+  `detail` text,
   PRIMARY KEY (`userID`),
   KEY `fieldID_to_User_fk_company` (`fieldID`),
   KEY `UserType_to_User_fk_company` (`userTypeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -137,10 +138,10 @@ CREATE TABLE IF NOT EXISTS `User` (
 --
 
 CREATE TABLE IF NOT EXISTS `UserType` (
-  `userTypeID` int(11) NOT NULL,
+  `userTypeID` int(11) NOT NULL AUTO_INCREMENT,
   `userType` varchar(50) NOT NULL,
   PRIMARY KEY (`userTypeID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Constraints for dumped tables
@@ -157,9 +158,9 @@ ALTER TABLE `Answer`
 -- Constraints for table `Comment`
 --
 ALTER TABLE `Comment`
-  ADD CONSTRAINT `Questionid_to_comment` FOREIGN KEY (`questionID`) REFERENCES `Question` (`questionID`),
   ADD CONSTRAINT `Answer_ID_fk_Comment` FOREIGN KEY (`answerID`) REFERENCES `Answer` (`answerID`),
-  ADD CONSTRAINT `Comment_userID_FK` FOREIGN KEY (`userID`) REFERENCES `User` (`userID`);
+  ADD CONSTRAINT `Comment_userID_FK` FOREIGN KEY (`userID`) REFERENCES `User` (`userID`),
+  ADD CONSTRAINT `Questionid_to_comment` FOREIGN KEY (`questionID`) REFERENCES `Question` (`questionID`);
 
 --
 -- Constraints for table `Question`
@@ -173,8 +174,8 @@ ALTER TABLE `Question`
 -- Constraints for table `User`
 --
 ALTER TABLE `User`
-  ADD CONSTRAINT `userType_to_user` FOREIGN KEY (`userTypeID`) REFERENCES `UserType` (`userTypeID`),
-  ADD CONSTRAINT `FieldID_to_User` FOREIGN KEY (`fieldID`) REFERENCES `Field` (`fieldID`);
+  ADD CONSTRAINT `FieldID_to_User` FOREIGN KEY (`fieldID`) REFERENCES `Field` (`fieldID`),
+  ADD CONSTRAINT `userType_to_user` FOREIGN KEY (`userTypeID`) REFERENCES `UserType` (`userTypeID`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
