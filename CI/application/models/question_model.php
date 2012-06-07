20e1f4ac13b4353c50b38db452509cc497d763ad
Alizai <alizai@Alizai-pc.(none)>
2012-06-08 01:27:48 +0200

    question_model.php

--- CI/application/models/question_model.php
+++ CI/application/models/question_model.php
@@ -3,166 +3,90 @@ class Question_model extends CI_Model {
 	   
    
 	 
-	    function __construct()
-   	 {
-        parent::__construct();
+	function __construct()   	 {
+        	parent::__construct();
    	 }
- function table_exists($table_name = null)
-   	 {
-    	return $this->db->table_exists($table_name);
-   	 }
-	   /**
-* ask_question method creates a record in the Question table.
-*
-* Option: Values
-* --------------
-*questionID
-*name
-*postedDate
-*body
-*rank
-*KEY catID
-*KEY fieldID
-*KEY userID
-*KEY commentID
-* 
-* @param array $options
-*/
-function ask_question($data)
-	{
-    
-   
-    // Execute the query
-    $this->db->insert('Questions',$data);
+ 	
 
-    // Return the ID of the inserted row, or false if the row could not be inserted
-    return $this->db->insert_id();
+	 // This method will return the limited quetion start from the offset order by questionID
+	 function get_question_list($limit,$offset){
+		 $this->db->order_by('questionID','asc');
+		 $query=$this->db->get($this->'Question', $limit, $offset);
+		return $query->result();
+  	}
+
+
+	// This method return limited number of quesiton belonging to specific user started from offset order by Question rank
+	 function get_question_list_userID($limit,$offset,$userID){
+		$this->db->order_by('postedDate','desc');
+		$this->db->where('userID',$userID);
+		 $query=$this->db->get($this->'Question',$limit,$offset);
+		return $query->result();
+
+	 }
+	 /**
+	* ask_question method creates a record in the Question table.
+
+	* @param array $options
+	*/
+	function ask_question($data){	     
+	    $this->db->insert('Questions',$data);
+	    return $this->db->insert_id();
 	}
 
-/**
-* update_answer method alters a record in the Question table.
-*
-* Option: Values
-* --------------
-*questionID
-*name
-*postedDate
-*body
-*rank
-*KEY catID
-*KEY fieldID
-*KEY userID
-*KEY commentID
-*
-* @param array $options
-* @return int affected_rows()
-*/
-function update_question($options = array())
-{
-    // required values
-    if(!$this->_required(array('body'), $options)) return false;
+	/**
+	* update_answer method alters a record in the Question table.
 
-    // qualification (we're not allowing to update data that it shouldn't)
-    $qualificationArray = array('name','rank','body','catID','fieldID','userID');
-    foreach($qualificationArray as $qualifier)
-    {
-        if(isset($options[$qualifier])) $this->db->set($qualifier, $options[$qualifier]);
-    	}
-
-    $this->db->where('questionID', $options['questionID']);
-
-    
-    // Execute the query
-    $this->db->update('Question');
-
-    // Return the number of rows updated, or false if the row could not be inserted
-    return $this->db->affected_rows();
+	* @param array $options
+	* @return int affected_rows()
+	*/
+	public function update_question($user_data, $questionID) {
+			$query = $this->db->update_string("Question", $user_data, $questionID);
+			$this->db->query($query);
+		
 	}
 
-/**
-*get_ten_recent_question_data will return 10 recent questions
-* --------------------------
-*questionID
-*name
-*postedDate
-*body
-*rank
-*KEY catID
-*KEY fieldID
-*KEY userID
-*KEY commentID
-*
-*SELECT * FROM table ORDER BY date DESC LIMIT 10;
-* @return array result() 
-*/
-function get_ten_recent_question($limit,$offset)
-	{	
-	//$this->db->select(' body,postedDate');
-    //$this->db->order_by('postedDate', 'desc');
-	//$this->db->limit(10);
-	$query = $this->db->get('Question',$limit, $offset);
-	
-
-   
-        //  it will return  array of objects from question type 
-        return $query->result_array();
-    
+	/**
+	*get_ten_recent_question will return 10 recent questions
+	*SELECT * FROM table ORDER BY date DESC LIMIT 10;
+	* @return array result() 
+	*/
+	function get_ten_recent_question($limit){	
+		$this->db->select(' body,postedDate');
+	    	$this->db->order_by('postedDate', 'desc');
+		$this->db->limit($limit);
+		$query = $this->db->get('Question');
+		return $query->result();
+	    
 	}
-/**
-*method search_question will return question near to search term 
-* --------------------------
-*SELECT * FROM table ORDER BY date DESC LIMIT 10;
-* @return array result() 
-* @parameter searchTerm
-*/
-function search_question($searchTerm)
-	{
-	$this->db->select(' body');
-	$this->db->like('body', $searchTerm);
-	$query = $this->db->get('Question');
-	return $query->result();
+	/**
+	*method search_question will return question near to searchTerm 
+	*/
+	function search_question($searchTerm){
+		$this->db->select(' body');
+		$this->db->like('body', $searchTerm);
+		$query = $this->db->get('Question');
+		return $query->result();
 
 	}
 
-/**
-*get_relevant_answers_for_question will returns all relevent answers to the question with  given questionID 
-* --------------------------
-*
-* @return array result() 
-* @parameter questionID
-*/
 
-// delete from Answer where answer.UserID=userid and Answer.questionID=quid
-
-function get_relevant_answers($questionID)
-	{
-	$this->db->select(' body');
-	$this->db->where('questionID', $questionID); 
-	$query = $this->db->get('Answer');
-	return $query->result();
+	/**
+	* delete_question method removes a record from the Question table
+	*
+	*/
+	public function delete_question($questionID){
+       
+	    $this->db->where('questionID', $questionID);
+	    $this->db->delete('Question');
 	}
-
-
-
-
-/**
-* delete_question method removes a record from the Question table
-*
-*/
-function delete_question($questionID)
-	{
-        
-
-    $this->db->where('questionID', $questionID);
-    $this->db->delete('Question');
-	}
-
-function delete_own_question($questionID,$userID){
-	$this->db->select('questoinID,userID');
-	$this->db->where('questionID',$questionID );
- 	$this->db->where('userID',$userID);
-	$query->db->get('Question');
-	if($query==null){
+	// this method is if aperson wants to delete it is own question 
+	public function delete_own_question($questionID,$userID){
+		$this->db->select('questoinID,userID');
+		$this->db->where('questionID',$questionID );
+ 		$this->db->where('userID',$userID);
+		$query->db->get('Question');
+		if($query==null){
 		return false;
 		}
 		else{
@@ -170,7 +94,22 @@ function delete_own_question($questionID,$userID){
 		}
 
 	}
+	
+	//This method will return the rank of specified questionID
+	public function get_rank($questionID){
+		$this->db->select('rank');
+		$this->db->where('questionID',$questionID);
+		$query=$this->db->get('Question');
+		return $query->resutl();
+	}
+	
+	// This method will add one to the current rank of the specified questionID
+	public function add_rank($questionID){
+		$currentRank=get_rank($questionID);
+		$this->db->query('update Answer set rank='.($currentRank+1).' where answerID='.$questionID);
 		
+	}
+	
 	
 	
 }// end of file
