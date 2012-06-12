bd1bf6991e4fafc5df5d7ee0fecfa594e5a40dad
Alizai <alizai@Alizai-pc.(none)>
2012-06-12 14:41:25 +0200

    user_type_model.php

--- CI/application/models/user_type_model.php
+++ CI/application/models/user_type_model.php
@@ -3,68 +3,35 @@ class User_type_model extends CI_Model {
 	   
    
 	 
-	    function __construct()
-    	{
+	function __construct(){
         parent::__construct();
     	}
- function table_exists($table_name = null)
-    	{
-    	return $this->db->table_exists($table_name);
-    	}
-	   /**
+ 
 
-* 
-* @param array $options
-*/
-function add_user_type($data)
-	{
-    
-
-    // Execute the query
-    $this->db->insert('UserType',$data);
-
-    // Return the ID of the inserted row, or false if the row could not be inserted
-    return $this->db->insert_id();
+	function add_user_type($userType){	    
+	     $this->db->set('userType', $userType);
+	     $this->db->insert('UserType'); 
+	     return $this->db->insert_id();
 	}
 
-/**
+	//this function will update the userType to the new name 
+	function update_user_type($userTypeID,$userTypeName){   
+	    $this->db->where('userTypeID', $userTypeID);
+	    $this->db->set('userType', $userTypeName);	   
+	    $this->db->update('UserType');
 
-*/
-function update_user_type($userTypeID,$userTypeName)
-	{
-   
-
-    $this->db->where('userTypeID', $userTypeID);
-
-   
-    $this->db->update('UserType',$userTypeName);
-
-    // Return the number of rows updated, or false if the row could not be inserted
-    return $this->db->affected_rows();
 	}
 
-/**
-
-*/
-function get_user_type_data()
-	{
-    
-   	 $query = $this->db->get('UserType');
-    
-         return $query->result();
-   
+	// this function will return all userType 
+	function get_user_type_data(){    
+	    $query = $this->db->get('UserType');    
+	    return $query->result();
+	   
 	}
 
-/**
-* DeleteUser method removes a record from the users table
-*
-* @param array $options
-*/
-function delete_user_type($userTypeID)
-	{
-    
-
-    $this->db->where('userTypeID', $userTypeID);
-    $this->db->delete('UserType');
+	// this function is for delation of user type with given userTypeID
+	function delete_user_type($userTypeID){   
+	    $this->db->where('userTypeID', $userTypeID);
+	    $this->db->delete('UserType');
 	}
-}
+}//end of file 
