f96b8f76b454ce6fd9766da39a9bf2ce03d5b38b
tilman <tilman@thinkpad.(none)>
2012-06-15 21:38:19 +0200

    remove search

--- application/views/leftnav/default.php
+++ application/views/leftnav/default.php
@@ -6,10 +6,4 @@
 ?>
 </ul>
 
-<form id="form">
-	<input type="text" value="Search..." onfocus="if (this.value == 'Search...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Search...';}" />
-	<input type="button" class="submit" value="Go" />
-</form>
-
-
 </div> <!-- leftnav -->
