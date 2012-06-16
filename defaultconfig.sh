e391d8eaeccfd999f86ada2fcfb3df0ab92853ec
Tilman <Tilman@Tilman-PC>
2012-06-16 16:17:29 +0200

    added backup of important config files

---
+++ defaultconfig.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp .htaccess.default .htaccess
+cp application/config/config.php.default application/config/config.php
+cp application/config/database.php.default application/config/database.php
