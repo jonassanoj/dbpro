3f796caed71b56d7b3b26af427e6f736ef553439
Alizai <alizai@Alizai-pc.(none)>
2012-06-17 11:44:36 +0200

    user_model.php

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -25,9 +25,43 @@ class User_model extends CI_Model {
 	}
 		
 	// TODO: implement the add_user function. A new unconfirmed user (userTypeID=0) is created with the given parameters. Set the accountCreationDate to the current date. Document the function using phpdoc.   
+	/**
+	 ** function add user is for adding new user in the table User with initial data *(username,password and email address)	
+	 * @author ASHUQULLAH ALIZAI
+	 * @param string_type $name is the username for the user 
+	 * @param password_type $password password specify by user
+	 * @param email_type $email email specify by user 
+	 */
 	public function add_user($name, $password, $email) {
 	// check for existing user
-	// return true if successful
+	$check_user =$this->_check_userName($name);
+	if (!$check_user){
+		return false;
+	}else {
+		$date = date('Y/m/d H:i:s');
+		$this -> db -> set('userName', $name);
+		$this -> db -> set('paswword', $password);
+		$this -> db -> set('email', $email);
+		$this -> db -> set('accountCreationDate', $date);
+		$this -> db -> insert('User');
+		// return true if successful
+		return true;
+	}
+	
+	}
+	/**
+	 * @author ASHUQULLAH ALIZAI
+	 * @param string_type $username is the user name for user who wants to register for first time 
+	 * *private function _check_userName checks if usename exists or not ,
+	 * @ returns false if username exist in the database, retruns true if the username not exist 
+	 */
+
+	public function _check_userName($username) {
+		$res = $this->db->get_where("User", $username)->result();
+		if(isset($res) && count($res) > 0 )
+			return false; // if user exists
+		else
+			return true;  // if not exists
 	}
 	
 
