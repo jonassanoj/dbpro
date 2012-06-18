326ad2ac2ea8f35e703468571687250f82e977e2
university <sayedsahm@yahoo.com>
2012-06-19 00:00:22 +0200

    Field_Model Documentation

--- application/models/field_model.php
+++ application/models/field_model.php
@@ -1,23 +1,56 @@
 <?php
 
-// TODO: complete field_model documentation according to the guidelines
+	/**
+	 * this model is for fields
+	 * 
+	 * Field_model: this class provide interfaces for geting
+	 * adding, deleting fields, from database, (Field) table
+	 * 
+	 */
 
 class Field_model extends CI_Model {
 
-	// get_field_data method returns an array of field record objects.
+	
+	
+	/**
+	 * this function is for geting field
+	 * 
+	 * get_fields(): this function select all rows from Field table
+	* 
+	* @return: Array: it return an array of all rows in the Field table
+	*/
+	
 	function get_fields() {
 		$query = $this -> db -> get('Field');
 		return $query -> result();
 	}
 
 
-	// add_field method creates a record in the Field table.
+
+	/**
+	 * it adds records in the field table
+	 * 
+	 * add_field(): This fuction add a new field into Field table
+	 * 
+	 * @param string $fieldName (String: needs a parameter which represent a field name)
+	 * @return int new_inserted_id (int: it return the new iserted field id)
+	 */
+	
 	function add_field($fieldName) {
 		$this -> db -> insert('Field', $fieldName);
 		return $this -> db -> insert_id();
 	}
 
-	// delete_field method removes a record from the field table.
+	
+	/**
+	 *deletes the feilds
+	 * 
+	 * delete_field(): This fuction delete a field from field table
+	 * 						Function takes an arguments which will be fieldID
+	 * @param int $fieldID (int: ID of the field to be delete)
+	 * @return: void
+	 */
+	
 	function delete_field($fieldID) {
 		$this -> db -> where('fieldID', $fieldID);
 		$this -> db -> delete('Field');
