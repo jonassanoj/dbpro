1f6989ec0b28363e39eae324eea203de1d9730b2
Wazir Khan <wazir.ahmadzai@gmail.com>
2012-06-19 15:55:26 +0200

    My task has finished, updating the failed method, and documenting the login, logout and failed method

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -53,22 +53,23 @@ class User extends CI_Controller {
 	
 	//TODO: Document the login(), logout() and failed() function. Change the implementation of failed() so it accepts only 3 failed password logins. After the third failed login the user should be sent to the recover page (user/recover/$username).
 	
-	/***
+	/**
 	* function for user login:
-	* 1: Getting values (username, password) from login form and checking it in database
+	* 
+	* This function check the following condition, and perform the task
+	*  <ul><li> Getting values (username, password) from login form and checking it in database
 	*    also login form have an optional checkbox field (remember) this function also check
-	*    if someone check this remember  checkbox, so it will create a cookie on client computer
-	*    which will be exist till 3 days, otherwise cookies will be delete
-	* 2: if user login failed so a failed(username) function will be called and this failed fuction 
+	*    if someone checked this remember  checkbox, so it will create a cookie on client computer
+	*    which will be exist till 3 days, otherwise cookies will be delete </li>
+	*  <li>if user login failed so a failed(username) function will be called and this failed fuction 
 	*    is counting that how much time login is failed if it was more than 3 times and new recover page
-	*    wil open to user. 
-	* 3: if user login successfully session data will be set for this user where 
-	*    userid, username, usertype, login(true, false) will store
-	* 4: when user data set in the sessioin the user will be redirect to the last visiting place
-	*    which will take from session data. (last_visited)
+	*    wil open to user.</li> 
+	*  <li>if user login successfully session data will be set for this user where 
+	*    userid, username, usertype, login(true, false) will store </li>
+	*  <li>when user data set in the sessioin the user will be redirect to the last visiting place
+	*    which will take from session data. (last_visited)</li></ul>
 	*
-	* @param: void
-	* @return: void
+	* @return void
 	*/
 	public function login()
 	{
@@ -98,14 +99,14 @@ class User extends CI_Controller {
 		redirect($this -> session -> userdata('last_visited'));
 	}
 	
-	/***
-	* Fuction for loging out the current login user
-	* Session data of the current user will unset here
-	* login, userid(uid), usertype, username
+	/**
+	* Fuction for loging out:
+	* 
+	* Session data of the current user will unset here:
+	* <ul><li>login</li> <li>userid(uid)</li> <li>usertype</li> <li>username</li></ul>
 	* will redirect after loging out, to the last visited place
 	* 
-	* @param: void
-	* @return: void
+	* @return void
 	*/
 	public function logout()
 	{
@@ -116,19 +117,21 @@ class User extends CI_Controller {
 		redirect($this -> session -> userdata('last_visited'));
 	}
 	
-	/***
-	* Function to count userlogin failing and to show login failed page, or account recover page
-	* It checks wheather the failed_logins variable set in the session data or not
-	* if not, it make a failed_logins index and storing 1st time login fail
-	* or if the failed_logins was already set in the session data it just increment it (last_value + 1)
+	/**
+	* Function to count userlogin failing:
+	* 
+	* This funciton is for counting user login failing and to show login failed page, or account recover page
+	* It checks wheather the failed_logins variable set in the session data or not if not, 
+	* it make a failed_logins index and storing 1st time login fail or if the failed_logins 
+	* was already set in the session data it just increment it (last_value + 1)
 	* than it set a data array to store error and username there, 
-	* Note: Error message taken from Language helper class, lang('message_index'); it return the specified error
-	* message in current language.
 	* This function also check if the user login failed less than 3 times it will redirect to the login_failed page
 	* and if the login failed 3 times it will redirect user to recover page, where he/she can recoreved his/her password
+	* @Note: Error message taken from Language helper class, lang('message_index'); it return the specified error
+	* message in current language.
 	*
-	* @param: $user (username, default value = '')
-	* @return: void
+	* @param String $user it represent username default-value = ''
+	* @return void
 	*/
 	public function failed($user='')
 	{
