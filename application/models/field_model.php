400b6c07218143c9a2b1e3ee051f57b8855e490f
Naseri <naseri@Naseri-Ubuntu.(none)>
2012-06-19 16:59:52 +0200

    Field_model documentations is done!

--- application/models/field_model.php
+++ application/models/field_model.php
@@ -4,8 +4,10 @@
 /**
  * the Field_model 
  *
- * Provides CRD functionality for Field.
- *
+ * Provides the following functions:
+ * _get_fields(): returns the field recoreds from table _Field.
+ * _add_field($fieldName): insert a new field recored to table _Field.
+ * _delete_field($fieldID): deletes a record from table _Field.
  * It uses the table _Field.
  *
  * @package models
@@ -14,23 +16,20 @@ class Field_model extends CI_Model {
 
 
 	/**
-	 * retrives all records from table Field as a record objects.
+	 * retrives all records from table Field as record objects.
 	 * @param none
 	 * @return array holds fields objects with fieldIDs and fieldNames. 
 	 */ 
-
 
 	function get_fields() {
 		$query = $this -> db -> get('Field');
 		return $query -> result();
 	}
 
-
-
 	/**
-	 * insert a given fieldName into table Field. 
-	 * fieldname: can a string that indicates area of study or profession of a user. 
-	 * @param unknown_type $fieldName
+	 * insert a given fieldName into table _Field. 
+	 * fieldname: can be a string that indicates area of study or profession of a user. 
+	 * @param string $fieldName
 	 */
 
 	function add_field($fieldName) {
@@ -39,9 +38,9 @@ class Field_model extends CI_Model {
 	}
 
 	/**
-	 * Deletes a specific record from table Field that matches the given fieldID. 
+	 * Deletes a specific record from table _Field that matches the given fieldID. 
 	 * fieldID: can be an integer that has beent from coller function.
-	 * @param unknown_type $fieldID
+	 * @param int $fieldID
 	 */
 
 	function delete_field($fieldID) {
