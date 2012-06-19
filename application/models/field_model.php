2a3a523779abb882e3dbc9964079822b268f26a1
Naseri <naseri@Naseri-Ubuntu.(none)>
2012-06-19 14:58:28 +0200

    Field_model class and its functions are documented.

--- application/models/field_model.php
+++ application/models/field_model.php
@@ -1,23 +1,43 @@
 <?php
 
-// TODO: complete field_model documentation according to the guidelines
 
+/**
+ * the Field_model 
+ *
+ * Provides CRD functionality for Field.
+ *
+ * It uses the table _Field.
+ *
+ * @package models
+ */
 class Field_model extends CI_Model {
 
-	// get_field_data method returns an array of field record objects.
+	/**
+	 * retrives all records from table Field as a record objects.
+	 * @param none
+	 * @return array holds fields objects with fieldIDs and fieldNames. 
+	 */ 
 	function get_fields() {
 		$query = $this -> db -> get('Field');
 		return $query -> result();
 	}
 
 
-	// add_field method creates a record in the Field table.
+	/**
+	 * insert a given fieldName into table Field. 
+	 * fieldname: can a string that indicates area of study or profession of a user. 
+	 * @param unknown_type $fieldName
+	 */
 	function add_field($fieldName) {
 		$this -> db -> insert('Field', $fieldName);
 		return $this -> db -> insert_id();
 	}
 
-	// delete_field method removes a record from the field table.
+	/**
+	 * Deletes a specific record from table Field that matches the given fieldID. 
+	 * fieldID: can be an integer that has beent from coller function.
+	 * @param unknown_type $fieldID
+	 */
 	function delete_field($fieldID) {
 		$this -> db -> where('fieldID', $fieldID);
 		$this -> db -> delete('Field');
