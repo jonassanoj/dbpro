28d95685ba06bd25231b42ee486715ef415ff4c9
ahmadnawid mustafazada <ahmadnawid.mz@gmail.com>
2012-06-19 17:14:34 +0200

    The _loadview function extended.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -36,7 +36,14 @@ class Main extends CI_Controller {
 	 *
 	 * * _include/header.php_
 	 * * _header/loginbox | loggedin : a header with loginbox or with logout button
-	 * * _leftnav/default.php_: the default content of the navigation bar
+	 * * _leftnav/_:
+	 * * ***If the user type is known, show specific leftnav bar depending on usertype.
+	 * ***usertype 1 for confirmed user
+	 * ***usertype 2 for editor
+	 * ***usertype 3 for admin
+	 * ***usertype 0 or else for unconfirmed user
+	 * * ***If the user type is unknown, show default or unconfirmed user leftnav bar.
+	 * ******************************************************************
 	 * * _body_/$body_view_: the body content given as a parameter
 	 * * _include/footer.php_
 	 *
@@ -46,7 +53,7 @@ class Main extends CI_Controller {
 	 *
 	 */
 
-	//TODO: extend the _loadviews() function so it loads a different sidebar, depending on the type of user.
+	
 	public function _loadviews($body_view, $data) {
 		// remember the current URL for creating backlinks
 		$this -> session -> set_userdata('last_visited', current_url());
@@ -61,7 +68,29 @@ class Main extends CI_Controller {
 			$this -> load -> view('header/loginbox',$data);
 		}
 			
-		$this -> load -> view('leftnav/default');
+		if($this -> session -> userdata('usertype')){
+		
+			if ($this -> session -> userdata('usertype') == 1)
+			{
+				$this -> load -> view('leftnav/user');
+			}
+			elseif ($this -> session -> userdata('usertype') == 2)
+			{
+				$this -> load -> view('leftnav/editor');
+			}
+			elseif ($this -> session -> userdata('usertype') == 3)
+			{
+				$this -> load -> view('leftnav/admin');
+			}
+			else
+			{
+				$this -> load -> view('leftnav/default');
+			}
+		}
+		else{
+			$this -> load -> view('leftnav/default');
+		}
+		
 		$this -> load -> view('body/' . $body_view, $data);
 		$this -> load -> view('include/footer');
 	}
