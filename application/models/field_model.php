c56957a9429284ee6fda00165e209922f42df7eb
Naseri <naseri@Naseri-Ubuntu.(none)>
2012-06-19 17:31:16 +0200

    field_model is documented

--- application/models/field_model.php
+++ application/models/field_model.php
@@ -14,27 +14,12 @@
  */
 class Field_model extends CI_Model {
 
-<<<<<<< HEAD
-
 	/**
 	 * retrives all records from table Field as record objects.
 	 * @param none
 	 * @return array holds fields objects with fieldIDs and fieldNames. 
 	 */ 
 
-=======
-	
-	
-	/**
-	 * this function is for geting field
-	 * 
-	 * get_fields(): this function select all rows from Field table
-	 * 
-	 * @author Sayed Ahmad Mahbobi
-	 * @return: Array: it return an array of all rows in the Field table
-	 */
-	
->>>>>>> 790cc173d628cdc5425520b2c1b81ad511ae3e9e
 	function get_fields() {
 		$query = $this -> db -> get('Field');
 		return $query -> result();
@@ -51,24 +36,13 @@ class Field_model extends CI_Model {
 		return $this -> db -> insert_id();
 	}
 
-<<<<<<< HEAD
+
 	/**
 	 * Deletes a specific record from table _Field that matches the given fieldID. 
 	 * fieldID: can be an integer that has beent from coller function.
 	 * @param int $fieldID
-=======
-	
-	/**
-	 *deletes the feilds
-	 * 
-	 * delete_field(): This fuction delete a field from field table
-	 * Function takes an arguments which will be fieldID
-	 * 
-	 * @author Sayed Ahmad Mahboobi
-	 * @param int $fieldID (int: ID of the field to be delete)
-	 * @return: void
->>>>>>> 790cc173d628cdc5425520b2c1b81ad511ae3e9e
-	 */
+
+			 */
 
 	function delete_field($fieldID) {
 		$this -> db -> where('fieldID', $fieldID);
