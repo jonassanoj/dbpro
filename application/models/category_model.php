720a1da52d69496f9ef2b537d21b12eba173d1ce
akbary <abdulaziz.akbary@mail.tu-berlin.de>
2012-06-20 21:22:57 +0200

    complete documentation of category model

--- application/models/category_model.php
+++ application/models/category_model.php
@@ -23,12 +23,11 @@ class Category_model extends CI_Model {
 	 /**
 	 * Add new category.
 	 * 
-	 * This function will add new category to the Category tables and a specified field table Category tables.
+	 * This function will add new category to a specified field in Category tables and return category ID.
 	 *
-	 * @author Abdulaziz Akbary
-	 * @param string $catName the catName, means category-Name.
-	 * @param int $fieldID the fieldID, means field-ID.  
-	 * @return int containing a category ID.
+	 * @param string $catName Means category-Name.
+	 * @param int $fieldID Means field-ID.  
+	 * @return int Category-ID of new inserted category.
 	 */
 
 	function add_cat($catName, $fieldID) {
@@ -39,16 +38,14 @@ class Category_model extends CI_Model {
 		return $this -> db -> insert_id();
 	}
 
-
 	 /**
 	 * Update category.
 	 * 
 	 * This function is used to update previouse existed category in a Category tables.
 	 *
-	 * @author Abdulaziz Akbary
-	 * @param int $catID the catID, means category-ID.
-	 * @param string $catName the catName, means category-Name.
-	 * @return int containing a category ID.
+	 * @param int $catID Means category-ID.
+	 * @param string $catName Means category-Name.
+	 * @return int Category-ID of updated category.
 	 */
 	function update_cat($catID, $catName) {
 		$this -> db -> where('catID', $catID);
@@ -60,11 +57,10 @@ class Category_model extends CI_Model {
  	 /**
 	 * Retrieving all category.
 	 * 
-	 * This function will return all categories belongs to a specific field from Category tables.
+	 * This function will return all categories thats belongs to a specific field from Category tables,if no parameter passed to this function in this case it will return the whole category table.
 	 *
-	 * @author Abdulaziz Akbary
-	 * @param int $fieldID the fieldID, means field-ID.
-	 * @return object containing all columns and rows of Category table.
+	 * @param int $fieldID Means field-ID.
+	 * @return array Containing columns and rows of Category table.
 	 */
 
 	
@@ -72,21 +68,22 @@ class Category_model extends CI_Model {
 		if (!$fieldID=0) $this -> db -> where('fieldID', $fieldID);
 		$query = $this -> db -> get('Category');
 		return $query -> result();
+			
 	}
 
-	 
 	 /**
 	 * Delete a category.
 	 * 
 	 * This function is used to delete a specific category from a Category tables.
 	 *
 	 * @author Abdulaziz Akbary
-	 * @param int $catID the catID, means category-ID.
-	 * @return Void 
+	 * @param int $catID Means category-ID.
+	 * @return int Category-ID of deleted row.
 	 */
 	 
 	function delete_cat($catID) {
 		$this -> db -> where('catID', $catID);
 		$this -> db -> delete('Category');
+		return $this -> db -> affected_rows();
 	}
 }
