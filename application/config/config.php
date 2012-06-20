a154e9d23770398a363b15602da52384aea19c95
ashuqullah <alizai.csf@hotamil.com>
2012-06-20 11:27:41 +0200

    changed local config file

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= 'http://localhost/dbpro';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
