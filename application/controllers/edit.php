e6521bf4b9ae32f498e0c7c7d2013fd9fb1a132e
hamidkhanzai <hamid_khanzai@yahoo.com>
2012-06-25 15:27:58 +0200

    changes form question

--- application/controllers/edit.php
+++ application/controllers/edit.php
@@ -22,6 +22,8 @@ class Edit extends CI_Controller {
 		$this -> load -> model('question_model');
 		$this -> load -> model('answer_model');
 		$this -> load -> model('comment_model');
+		// load category model 
+		$this->load->model('category_model');		
 		// if no language defined in session, load default language.
 		if (!$this -> session -> userdata('language'))
 			$this -> lang -> load('main');
@@ -55,6 +57,90 @@ class Edit extends CI_Controller {
 	 * 
 	 */
 	public function question($qid=0){
+		
+		// load form validation
+		$this->load->library('form_validation');
+		// setting validation rules for form_question
+		$this->form_validation->set_rules('title','Question Title','trim|required|min_length[10]');
+		$this->form_validation->set_rules('body','Question Body','trim|required|min_length[20]');
+		$this->form_validation->set_rules('catID','Category','trim|required');
+		
+		// This method run only if validation rules have been followed 
+		if($this->form_validation->run()){
+		
+				if($this->session->userdata('action')=="edit"){
+					//echo	$this->session->userdata('qid').'hi';
+					if(isset($_POST['btn'])) unset($_POST['btn']);
+					if($this->question_model->update_question($this->session->userdata('qid'),$_POST)){
+						echo "Your Question successfully updated";
+						//redirect($this->session->userdata('last_visited'));
+						$this->session->unset_userdata('qid');
+					}
+				
+				}
+				else{//($title, $uid, $cid, $body) {
+					
+					if($this->question_model->create_question($_POST['title'],$this->session->userdata('uid'),$_POST['catID'],$_POST['body'])!=0){
+						echo "Your Question have been successfully created";
+					}
+				}
+			
+			}
+		// call function listCategory for the pupose of filling dropdownlist
+		$data['catList']=$this->listCategory();
+		// if quesiton id is greater then zero then it means to perform edite action 
+		if($qid>0){
+			$this->session->set_userdata('qid',$qid);
+			$this->session->set_userdata('action',"edit");
+			// load a question from question table 
+			$data['question']=$this->question_model->get_details($qid);
+			// checak if the $qid exist question table 
+			if($data){
+				// check if the question belong to the current user
+				if(true){ //$question->userID==$this->session->userdata('uid')
+						$_POST['title']='Edit Question';
+						$_POST['btn']='Save Changes';
+						$data['title']='Edit Question';
+						$this->_loadviews('form_question',$data);
+					}
+				
+				}
+				else{
+					// if the question does not belongs to the current user redirect the user to back to the last page
+					redirect($this->session->userdata('last_visited'));
+				}
+			}
+			// if the $qid is 0 then we diplay the form for creation fo question 
+			else{
+				$this->session->set_userdata('action',"save");
+				$data['question']=null; 
+				$_POST['titles']='Create Question';
+				$_POST['btn']='Save Question';
+				$this->_loadviews('form_question',$data);
+			} 
+		
+		}
+		
+		/**
+		 * Helper method which is used to slect category from the Category Table 
+		 * @param 
+		 * @return array of category 
+		 */
+		
+		function listCategory(){
+			$this->load->model('question_model');
+			$catData=$this->category_model->get_categories();
+			if($catData){
+			$catList=array();
+			foreach($catData as $row){
+				$catList[$row->catID]=$row->catName;
+			}
+			return $catList;
+			
+		}
+		
+		
+		
 		
 		//TODO: check if $qid is the user's question, otherwise send the user back where he came from (hint: see controllers/user.php for how to do this)
 		// show a form to edit a question if $qid does not exist it's empty, otherwise it should already contain the question's data
