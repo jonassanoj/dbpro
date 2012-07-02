ef5bcdf2e9ba18bff415b20c1fa09995cfd27e57
tilman <tilman@thinkpad>
2012-07-02 12:41:50 +0200

    updated docs

--- createdoc.sh
+++ createdoc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-phpdoc -d application/models -d application/controllers -d application/helpers -t docs
+phpdoc -d application/models -d application/controllers -d application/helpers -i application/helpers/markdown_helper.php -t docs
